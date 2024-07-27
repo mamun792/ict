b802e682eab500b1d4da449da73cef140d1843ec
Mahabubur Rahaman <mahababurrahaman2014@gmail.com>
2024-07-27 22:49:10 +0600

    added html folde

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+   // following the variable name delectaion rule in C
+   // 1 - variable name should start with a letter or an underscore
+    // 2 - variable name should not start with a number
+    // 3 - variable name should not contain any special character except underscore
+    
+   
+    return 0;
+}
